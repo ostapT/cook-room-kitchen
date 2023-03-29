ffa7ac48433eb8036f71acf14f172284f188e5dc
Ostap <anastasiia@Anastasiias-Air.Dlink>
2023-03-29 14:11:19 +0300

    Add index view

--- kitchen/models.py
+++ kitchen/models.py
@@ -7,6 +7,12 @@ from cook_room_kitchen import settings
 class DishType(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=255)
@@ -15,8 +21,20 @@ class Dish(models.Model):
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="dishes")
 
+    class Meta:
+        ordering = ["dish_type"]
+
+    def __str__(self):
+        return f"{self.name} price: {self.price}"
+
 
 class Cook(AbstractUser):
     years_of_experience = models.IntegerField(null=True)
 
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Cook, Dish
+
+
+def index(request):
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+
+    context = {
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks
+    }
+    return render(request, "kitchen/index.html", context=context)
